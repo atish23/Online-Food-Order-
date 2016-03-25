c3c532b0338eab80b836c59c30df73307e43c23a
atish23 <amaske@delaplex.in>
2016-03-25 19:19:00 +0530

    Fixed slider issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
-//= require bootstrap.min
 //= require turbolinks
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160322111155) do
-
+ActiveRecord::Schema.define(version: 20160322133941) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
